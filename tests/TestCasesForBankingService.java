6b3b3ad6019a9767ee5f74618a2f34f126df75eb
ReinisKrumins <r.krumins9@gmail.com>
2022-05-13 12:13:10 +0300

    ADDED: very simple tests for day3 tasks

--- tests/TestCasesForBankingService.java
+++ tests/TestCasesForBankingService.java
@@ -9,26 +9,25 @@ public class TestCasesForBankingService {
     public void testDepositingMoney()
     {
         BankAccount bankAcc = new BankAccount();
-        BankAccount bankAcc2 = new BankAccount();
 
         bankAcc.deposit(6000);
-        bankAcc2.deposit(50);
-
         Assert.assertEquals(bankAcc.getBalance(), 0, 0);
-        Assert.assertEquals(bankAcc2.getBalance(), 50, 0);
+
+        bankAcc.deposit(499);
+        Assert.assertEquals(bankAcc.getBalance(), 499, 0);
     }
 
     @Test
     public void testWithdrawingMoney()
     {
         BankAccount bankAcc = new BankAccount(500);
-        BankAccount bankAcc2 = new BankAccount(20);
+
+        bankAcc.withdraw(600);
+        Assert.assertEquals(bankAcc.getBalance(), 500, 0);
 
         bankAcc.withdraw(150);
-        bankAcc2.withdraw(30);
-
         Assert.assertEquals(bankAcc.getBalance(), 350, 0);
-        Assert.assertEquals(bankAcc2.getBalance(), 20, 0);
+
     }
 
     @Test
@@ -36,15 +35,13 @@ public class TestCasesForBankingService {
     {
         BankAccount bankAcc = new BankAccount(500);
         BankAccount bankAcc2 = new BankAccount(20);
-        BankAccount bankAcc3 = new BankAccount(10);
-        BankAccount bankAcc4 = new BankAccount(60);
 
         MainBankService.transferMoney(bankAcc, bankAcc2, 600);
-        MainBankService.transferMoney(bankAcc3, bankAcc4, 5);
-
         Assert.assertEquals(bankAcc.getBalance(), 500, 0);
         Assert.assertEquals(bankAcc2.getBalance(), 20, 0);
-        Assert.assertEquals(bankAcc3.getBalance(), 5, 0);
-        Assert.assertEquals(bankAcc4.getBalance(), 65, 0);
+
+        MainBankService.transferMoney(bankAcc, bankAcc2, 500);
+        Assert.assertEquals(bankAcc.getBalance(), 0, 0);
+        Assert.assertEquals(bankAcc2.getBalance(), 520, 0);
     }
 }
--- tests/TestCasesForCarService.java
+++ tests/TestCasesForCarService.java
@@ -1,2 +1,53 @@
+import enums.Brand;
+import enums.Model;
+import model.Minivan;
+import org.junit.Assert;
+import org.junit.Test;
+import service.MainCarService;
+
 public class TestCasesForCarService {
+
+    @Test
+    public void testCarSteering(){
+        Minivan minivanOne = new Minivan(2000,
+                0 ,
+                "N" ,
+                "Straight",
+                Brand.Audi,
+                Model.alpha);
+
+        MainCarService.steering(minivanOne, "Straight");
+        Assert.assertEquals(minivanOne.getSteer(), "Straight");
+
+        MainCarService.steering(minivanOne, "Right");
+        Assert.assertEquals(minivanOne.getSteer(), "Right");
+    }
+    @Test
+    public void testCarGears(){
+        Minivan minivanOne = new Minivan(2000,
+                0 ,
+                "N" ,
+                "Straight",
+                Brand.Audi,
+                Model.alpha);
+
+        MainCarService.changingGears(minivanOne, "N");
+        Assert.assertEquals(minivanOne.getGears(), "N");
+        MainCarService.changingGears(minivanOne, "1");
+        Assert.assertEquals(minivanOne.getGears(), "1");
+    }
+    @Test
+    public void testCarSpeed(){
+        Minivan minivanOne = new Minivan(2000,
+                0 ,
+                "N" ,
+                "Straight",
+                Brand.Audi,
+                Model.alpha);
+
+        MainCarService.changeSpeed(minivanOne, 210);
+        Assert.assertEquals(minivanOne.getSpeed(), 0);
+        MainCarService.changeSpeed(minivanOne, 20);
+        Assert.assertEquals(minivanOne.getSpeed(), 20);
+    }
 }
